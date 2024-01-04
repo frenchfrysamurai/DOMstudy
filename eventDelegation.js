e961cc4d366b7a1b1c80b866fe76c8c95cc5d756
Emilio Rodriguez <frenchfrysamurai@gmail.com>
2024-01-03 22:46:37 -0600

    Moving onto part 2 - project 1

---
+++ eventDelegation.js
@@ -0,0 +1,26 @@
+// Event Delegation
+//
+// It allows users to apend a SINGLE event listener to a parent element that adds it to all of its present
+// AND future descendants that match a selector
+//
+
+document.querySelector('#sports').addEventListener
+('click', function(e){
+
+	console.log(e.target.getAttribute('id') + ' is clicked');
+
+	const target = e.target;
+
+	if (target.matches('li')) {
+		target.style.backgroundColor = 'lightgrey';
+	}
+})
+
+
+const sports = document.querySelector('#sports');
+const newSport = document.createElement('li');
+
+newSport.innerText = 'rugby';
+newSport.setAttribute('id', 'rugby');
+
+sports.appendChild(newSport);
---
+++ pt2/project1.js
@@ -0,0 +1,8 @@
+// Variables
+//
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+
+let person = document.querySelector('.person');
+
+const quotes;
